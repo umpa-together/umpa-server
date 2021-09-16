0f0085c30d671e44a2757beb3afa32e3f612727c
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-16 16:15:34 +0900

    fix: add real Name in User model

--- src/models/User.js
+++ src/models/User.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    realName: {
+        type: String,
+        unique: true,
+    },
     name: {
         type: String,
         required:true,

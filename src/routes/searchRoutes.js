e8b70e73f0059ff81bd94a3ace6c4e6af40674e8
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-28 00:31:32 +0900

    refactor: query refactor in search hashtag, and delete unnecessary api

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -10,37 +10,45 @@ const requireAuth = require('../middlewares/requireAuth');
 const router = express.Router();
 router.use(requireAuth);
 
-router.get('/initPlaylist', async (req, res) => {
-    try {
-        const playlistNum= await Playlist.countDocuments();
-        res.send({playlistNum});
-    } catch (err) {
-        return res.status(422).send(err.message);
-    }
-});
-
-router.get('/searchSongOrArtist/:id', async (req, res) => {
-    try {
-        const foundPlaylist = []
-        const playList = await Playlist.find().populate('postUserId', {profileImage: 1});
-        for(let key in playList){
-            for(let song in playList[key].songs){
-                if(playList[key].songs[song].id == req.params.id){
-                    foundPlaylist.push(playList[key]);
-                    break;
-                }
-            }
-        }
-        res.send(foundPlaylist);
-    } catch (err) {
-        return res.status(422).send(err.message);
-    }
-});
-
 router.get('/searchHashtag/:object', async (req, res) => {
     try {
-        const result = await Hashtag.findOne({hashtag :req.params.object}).populate('playlistId').populate('dailyId');
-        res.send(result);
+        const hashtag = await Hashtag.aggregate([
+            { $match: { hashtag: req.params.object }},
+            { 
+                $lookup: {
+                    from: 'playlists',
+                    localField: 'playlistId',
+                    foreignField: '_id',
+                    as: 'playlist',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'dailies',
+                    localField: 'dailyId',
+                    foreignField: '_id',
+                    as: 'daily'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    hashtag: 1,
+                    'playlist._id': 1,
+                    'playlist.title': 1,
+                    'playlist.hashtag': 1,
+                    'playlist.image': 1,
+                    'playlist.postUserId': 1,
+                    'daily._id': 1,
+                    'daily.title': 1,
+                    'daily.hashtag': 1,
+                    'daily.image': 1,
+                    'daily.song': 1,
+                    'daily.postUserId': 1
+                }
+            }
+        ])
+        res.send(hashtag[0]);
     } catch (err) {
         return res.status(422).send(err.message);
     }
@@ -50,16 +58,22 @@ router.get('/searchAll/:id', async (req, res) => {
     try {
         const playlists = await Playlist.find({
             songs: { $elemMatch: { id: req.params.id }}
-        }, {image: 1, title: 1, hashtag: 1}).populate('postUserId', {name: 1, profileImage: 1})
+        }, {image: 1, title: 1, hashtag: 1}).populate('postUserId', { name: 1, profileImage: 1 })
         
         const dj = await User.find({
-            songs: { $elemMatch: { id: req.params.id}}
-        }, {profileImage: 1, name: 1, songs: 1})
+            songs: { $elemMatch: { id: req.params.id }}
+        }, { profileImage: 1, name: 1, songs: 1 })
         
+        const daily = await Daily.find({
+            'song.id': req.params.id 
+        }, { image: 1, song: 1 }).populate('postUserId', { name: 1, profileImage : 1 })
+
         const result = {
             playlists: playlists,
-            dj: dj
+            dj: dj,
+            daily: daily
         }
+
         res.send(result)
     } catch (err) {
         return res.status(422).send(err.message);
@@ -68,9 +82,43 @@ router.get('/searchAll/:id', async (req, res) => {
 
 router.get('/searchHashtagAll/:term', async (req, res) => {
     try {
-        const hashtag = await Hashtag.findOne({hashtag :req.params.term}).populate('playlistId', {image: 1, title: 1, hashtag: 1, postUserId: 1}).populate('dailyId', {image: 1});
-        const playlists = hashtag.playlistId
-        const daily = hashtag.dailyId
+        //const hashtag = await Hashtag.findOne({hashtag :req.params.term}).populate('playlistId', { image: 1, title: 1, hashtag: 1, postUserId: 1 }).populate('dailyId', { image: 1, song: 1, postUserId: 1 });
+        const hashtag = await Hashtag.aggregate([
+            { $match: { hashtag: req.params.term }},
+            {
+                $lookup: {
+                    from: 'playlists',
+                    localField: 'playlistId',
+                    foreignField: '_id',
+                    as: 'playlist'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'dailies',
+                    localField: 'dailyId',
+                    foreignField: '_id',
+                    as: 'daily'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    hashtag: 1,
+                    'playlist._id': 1,
+                    'playlist.title': 1,
+                    'playlist.hashtag': 1,
+                    'playlist.image': 1,
+                    'playlist.postUserId': 1,
+                    'daily._id': 1,
+                    'daily.image': 1,
+                    'daily.song': 1,
+                    'daily.postUserId': 1
+                }
+            }
+        ])
+        const playlists = hashtag[0].playlist
+        const daily = hashtag[0].daily
         const result = {
             playlists: playlists,
             daily: daily
@@ -83,7 +131,7 @@ router.get('/searchHashtagAll/:term', async (req, res) => {
 
 router.get('/hashtagHint/:term', async (req, res) => {
     try {
-        const hint = await Hashtag.find({hashtag: {$regex: `${req.params.term}`}}).populate('playlistId', {_id: 1}).populate('dailyId');
+        const hint = await Hashtag.find({hashtag: {$regex: `${req.params.term}`}}).populate('playlistId', { _id: 1 }).populate('dailyId', { _id: 1});
         let resultHint = [];
         for(let key in hint){
             if(hint[key].playlistId.length !== 0 || hint[key].dailyId.length !== 0)    resultHint.push(hint[key])
@@ -103,22 +151,45 @@ router.get('/djHint/:term', async (req, res) => {
     }
 });
 
-router.get('/searchDJ/:id', async (req, res) => {
-    try {
-        //const users = await User.find({'songs.id' : {$regex:`${req.params.id}`}});
-        const users = await User.find({'songs.id' : req.params.id});
-        const user = users.filter(user => user._id.toString() != req.user._id.toString());
-        res.send(user);
-    } catch (err) {
-        return res.status(422).send(err.message);
-    }
-})
-
 router.get('/currentHashtag', async (req, res) => {
     try {
-        const hashtag = await Hashtag.aggregate([{$sample: { size: 10 }}])
-        const resultHashtag = await Hashtag.populate(hashtag, {path: "playlistId"});
-        res.send(resultHashtag)
+        const hashtag = await Hashtag.aggregate([
+            { $sample: { size: 10 } },
+            { 
+                $lookup: {
+                    from: 'playlists',
+                    localField: 'playlistId',
+                    foreignField: '_id',
+                    as: 'playlist',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'dailies',
+                    localField: 'dailyId',
+                    foreignField: '_id',
+                    as: 'daily'
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    hashtag: 1,
+                    'playlist._id': 1,
+                    'playlist.title': 1,
+                    'playlist.hashtag': 1,
+                    'playlist.image': 1,
+                    'playlist.postUserId': 1,
+                    'daily._id': 1,
+                    'daily.title': 1,
+                    'daily.hashtag': 1,
+                    'daily.image': 1,
+                    'daily.song': 1,
+                    'daily.postUserId': 1
+                }
+            }
+        ])
+        res.send(hashtag)
     } catch (err) {
         return res.status(422).send(err.message);
     }

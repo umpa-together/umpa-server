66e8db6b751d17e9b2c7eea04325e8650488df1e
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-07 01:09:07 +0900

    delete: remove weekly curation, dj

--- src/models/Weekly.js
+++ src/models/Weekly.js
@@ -8,28 +8,6 @@ const weeklyPlaylistSchema = new mongoose.Schema({
     }
 });
 
-const WeeklyCurationSchema = new mongoose.Schema({
-    curation:[{
-        type:mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref:'Curation'
-    }],
-    time: {
-        type: String
-    },
-});
-
-const WeeklyDJSchema = new mongoose.Schema({
-    DJ:[{
-        type:mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref:'User'
-    }],
-    time: {
-        type: String
-    },
-});
-
 const WeeklyDailySchema = new mongoose.Schema({
     daily:[{
         type: mongoose.Schema.Types.ObjectId,
@@ -38,6 +16,4 @@ const WeeklyDailySchema = new mongoose.Schema({
 })
 
 mongoose.model('WeeklyPlaylist', weeklyPlaylistSchema);
-mongoose.model('WeekCuration', WeeklyCurationSchema);
-mongoose.model('WeekDJ', WeeklyDJSchema);
 mongoose.model('WeeklyDaily', WeeklyDailySchema)

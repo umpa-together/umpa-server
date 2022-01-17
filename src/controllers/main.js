dc086a66fd96f81965ece5a64e29ee92203ee02c
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2022-01-17 18:03:32 +0900

    fix: Delete unnecessary api, and Add recommend api, add ThemeLists

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,22 +2,42 @@ const mongoose = require('mongoose');
 const Playlist = mongoose.model('Playlist');
 const User = mongoose.model('User');
 const Daily = mongoose.model('Daily');
-const WeeklyPlaylist = mongoose.model('WeeklyPlaylist');
-const WeeklyDaily = mongoose.model('WeeklyDaily');
-const Hashtag = mongoose.model('Hashtag');
 const AddedSong = mongoose.model('AddedSong');
+const Theme = mongoose.model('Theme');
 
-// time fields string -> Date 변경
-const changeTime = async (req, res) => {
+const themeLists = [
+    '음파에서 추천하는 꼭 들어야 하는 플리',
+    '시원한 여름날이 생각나는 플리',
+    '무념무상 나는 이거 들을거야',
+    '작업하면서 틀어놔!',
+    '어디서 들어본 듯한 노래들이 요기에'
+]
+
+// 테마 데이터 등록하기
+const addThemeLists = async (req, res) => {
     try {
-        const weekly = await WeeklyPlaylist.find()
-        weekly.map(async (item) => {
-            const { _id: id, time } = item
-            await WeeklyPlaylist.findOneAndUpdate({
+        themeLists.map(async (theme) => {
+            await new Theme({
+                title: theme
+            }).save();
+        })
+        res.status(200).send();
+    } catch (err) {
+        return res.status(422).send(err.message);    
+    }
+}
+
+// 데일리 accessedTime 추가
+const accessedTime = async (req, res) => {
+    try {
+        const dailies = await Daily.find();
+        dailies.map(async (item) => {
+            const { _id: id } = item
+            await Daily.findOneAndUpdate({
                 _id: id
             }, {
                 $set: {
-                    time: new Date(time)
+                    accessedTime: new Date()
                 }
             })
         })
@@ -85,126 +105,32 @@ const getNextAllDailies = async (req, res) => {
 // 최근에 본 플리 가져오기
 const getRecentPlaylist = async (req, res) => {
     try {
-        const nowTime = new Date()
         const playlists = await Playlist.find({
             "accessedTime": {
                 $exists: true
             }
         }, {
-            title: 1, accessedTime: 1, image: 1, songs: 1
-        })
-        playlists.sort(function(a, b) {
-            if(nowTime.getTime() - a.accessedTime.getTime() > nowTime.getTime() - b.accessedTime.getTime())  return 1;
-            if(nowTime.getTime() - a.accessedTime.getTime() < nowTime.getTime() - b.accessedTime.getTime())  return -1;
-            return 0;
-        });
-        res.send(playlists.slice(0, 10))
+            title: 1, image: 1, songs: 1
+        }).sort({'accessedTime': -1}).limit(10)
+        res.send(playlists)
     } catch (err) {
         return res.status(422).send(err.message);   
     }
 }
 
-// 위클리 플리, 데일리 가져오기
-const getWeekly = async (req, res) => {
+// 최근에 본 데일리 가져오기
+const getRecentDaily = async (req, res) => {
     try {
-        const playlists = await Playlist.find({ 
-            isWeekly: true 
-        }, { 
-            title: 1, hashtag: 1, image: 1 
-        }).populate('postUserId', { 
-            name: 1, profileImage: 1 
-        })
-        const dailies = await Daily.find({ 
-            isWeekly: true 
-        }, { 
-            song: 1, image: 1 
-        }).populate('postUserId', { 
-            name: 1, profileImage: 1 
-        })
-        const result = [
-            playlists,
-            dailies
-        ]
-        res.send(result);
+        const dailes = await Daily.find({
+            "accessedTime": {
+                $exists: true
+            }
+        }, {
+            song: 1, postUserId: 1
+        }).sort({'accessedTime': -1}).limit(10)
+        res.send(dailes)
     } catch (err) {
-        return res.status(422).send(err.message);
-    }
-}
-
-// 위클리 생성
-const postWeekly = async (req, res) => {
-    const nowTime = new Date()
-    const weekly = await WeeklyPlaylist.find({
-        time: {
-            $gte: new Date(new Date().setDate(nowTime.getDate()-7))
-        }
-    })
-    if (weekly.length !== 0) {
-        return res.status(200).send()
-    } else {
-        try {
-            // update weekly 
-            const weeklyPlaylist = WeeklyPlaylist({
-                playlist: [], 
-                time: nowTime
-            });
-            const weeklyDaily = WeeklyDaily({
-                daily: [], 
-                time: nowTime
-            })
-            await Promise.all([
-                Playlist.updateMany({ isWeekly: false }),
-                Daily.updateMany({ isWeekly: false, nominate: 0 })
-            ])
-            const [playlist, daily] = await Promise.all([
-                Playlist.find({}, { likes:1 ,views: 1, nominate: 1 }),
-                Daily.find({}, { likes: 1, views: 1, nominate: 1})
-            ])
-            playlist.sort(function(a, b) {
-                if((a.likes.length+Math.sqrt(a.views))/Math.pow(a.nominate+1,2)  > (b.likes.length+Math.sqrt(b.views))/Math.pow(b.nominate+1,2))  return -1;
-                if((a.likes.length+Math.sqrt(a.views))/Math.pow(a.nominate+1,2)  < (b.likes.length+Math.sqrt(b.views))/Math.pow(b.nominate+1,2)) return 1;
-                return 0;
-            });
-            daily.sort(function(a, b) {
-                if((a.likes.length+Math.sqrt(a.views))/Math.pow(a.nominate+1,2)  > (b.likes.length+Math.sqrt(b.views))/Math.pow(b.nominate+1,2))  return -1;
-                if((a.likes.length+Math.sqrt(a.views))/Math.pow(a.nominate+1,2)  < (b.likes.length+Math.sqrt(b.views))/Math.pow(b.nominate+1,2)) return 1;
-                return 0;
-            })
-
-            const selectedPlaylist = playlist.slice(0,10);
-            selectedPlaylist.forEach(async (item, index) => {
-                try{
-                    weeklyPlaylist.playlist.push(item._id)
-                    await Playlist.findOneAndUpdate({ 
-                        _id: item._id 
-                    }, {
-                        $inc: { nominate: 1 }, 
-                        $set: { isWeekly: true }
-                    });
-                    if(index == selectedPlaylist.length-1)  await weeklyPlaylist.save();
-                } catch (err) {
-                    return res.status(422).send(err.message);
-                }
-            });
-            const selectedDaily = daily.slice(0, 10);
-            selectedDaily.forEach(async (item, index) => {
-                try {
-                    weeklyDaily.daily.push(item._id)
-                    await Daily.findOneAndUpdate({ 
-                        _id: item._id 
-                    }, { 
-                        $inc: { nominate: 1 }, 
-                        $set: { isWeekly: true } 
-                    });
-                    if(index == selectedDaily.length-1) await weeklyDaily.save()
-                } catch (err) {
-                    return res.status(422).send(err.message);
-                }
-            })
-            res.status(200).send()
-        } catch (err) {
-            return res.status(422).send(err.message);
-        }
+        return res.status(422).send(err.message);   
     }
 }
 
@@ -301,7 +227,6 @@ const getMainRecommendDJ = async (req, res) => {
                 _id: id,
                 score: userScore, 
                 genre: genre,
-                songs: songs[0],
                 name: name, 
                 profileImage: profileImage,
                 playlistCount: userPlaylists.length
@@ -328,11 +253,10 @@ const getMainRecommendDJ = async (req, res) => {
     }
 }
 
-
-const getCurrentHashtag = async (req, res) => {
+const getMainRecommendPlaylist = async (req, res) => {
     try {
-        const hashtag = await Hashtag.aggregate([
-            { $sample: { size: 10 } },
+        const theme = await Theme.aggregate([
+            { $sample: { size: 4 } },
             { 
                 $lookup: {
                     from: 'playlists',
@@ -342,46 +266,40 @@ const getCurrentHashtag = async (req, res) => {
                 },
             },
             {
-                $lookup: {
-                    from: 'dailies',
-                    localField: 'dailyId',
-                    foreignField: '_id',
-                    as: 'daily'
-                }
-            },
-            {
                 $project: {
-                    _id: 1,
-                    hashtag: 1,
+                    title: 1,
+                    image: 1,
                     'playlist._id': 1,
-                    'playlist.title': 1,
-                    'playlist.hashtag': 1,
-                    'playlist.image': 1,
                     'playlist.postUserId': 1,
-                    'daily._id': 1,
-                    'daily.title': 1,
-                    'daily.hashtag': 1,
-                    'daily.image': 1,
-                    'daily.song': 1,
-                    'daily.postUserId': 1
                 }
             }
         ])
-        res.send(hashtag)
+        let result = []
+        for (item of theme) {
+            const { title, playlist, image } = item
+            const idx = Math.floor(Math.random() * playlist.length);
+            result.push({
+                title: title,
+                playlist: playlist[idx],
+                image: image
+            })
+        }
+        res.status(200).send(result)
     } catch (err) {
-        return res.status(422).send(err.message);
+        return res.status(422).send(err.message); 
     }
 }
 
+
 module.exports = {
-    changeTime,
+    addThemeLists,
+    accessedTime,
     getAllPlaylists,
     getNextAllPlaylists,
     getAllDailies,
     getNextAllDailies,
     getRecentPlaylist,
-    getWeekly,
-    postWeekly,
+    getRecentDaily,
     getMainRecommendDJ,
-    getCurrentHashtag
+    getMainRecommendPlaylist,
 }
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const {
-    changeTime,
+    addThemeLists,
+    accessedTime,
     getAllPlaylists,
     getNextAllPlaylists,
     getAllDailies,
     getNextAllDailies,
     getRecentPlaylist,
-    getWeekly,
-    postWeekly,
+    getRecentDaily,
     getMainRecommendDJ,
-    getCurrentHashtag
+    getMainRecommendPlaylist
 } = require('../controllers/main')
 
-router.get('/time', changeTime);
+router.put('/theme', addThemeLists);
+router.get('/accessedTime', accessedTime);
 router.get('/playlist', getAllPlaylists);
 router.get('/playlist/:page', getNextAllPlaylists);
 router.get('/daily', getAllDailies);
 router.get('/daily/:page', getNextAllDailies);
-router.get('/recent', getRecentPlaylist);
-router.get('/weekly', getWeekly);
-router.post('/weekly', postWeekly);
-router.get('/recommendDJ', getMainRecommendDJ);
-router.get('/hashtag', getCurrentHashtag);
+router.get('/recent-playlists', getRecentPlaylist);
+router.get('/recent-dailies', getRecentDaily);
+router.get('/recommend-playlists', getMainRecommendPlaylist);
+router.get('/recommend-dj', getMainRecommendDJ);
 
 module.exports = router;

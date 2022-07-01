8be0fd5ea2fe31062953d62dc7db5a48bcfa5d65
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2022-07-01 15:51:02 +0900

    fix: Add diff in today and yesterday user count

--- src/utils/slackBot.js
+++ src/utils/slackBot.js
@@ -3,10 +3,18 @@ const User = mongoose.model('User')
 const Playlist = mongoose.model('Playlist')
 const Daily = mongoose.model('Daily')
 const { WebClient } = require('@slack/web-api')
+const fs = require('fs')
 
 const token = process.env.SLACK_TOKEN
 const channel = process.env.SLACK_CHANNEL
 const slackBot = new WebClient(token)
+
+const userCountPath = `${__dirname}/../../../daily_user_count.txt`
+
+const getPrevUserCount = () => {
+    const userCount = fs.readFileSync(userCountPath, 'utf8').toString().split('\n')
+    return parseInt(userCount[userCount.length-2].split(' ')[1])
+}
 
 const sendTodayData = async (req, res) => {
     try {
@@ -28,13 +36,15 @@ const sendTodayData = async (req, res) => {
                 $gt: prevTime
             }
         })
-        
-        const today = `${currentTime.getFullYear()}년 ${currentTime.getMonth() < 9 ? '0' + (currentTime.getMonth()+1) : currentTime.getMonth()+1}월 ${currentTime.getDate() < 10 ? '0' + currentTime.getDate() : currentTime.getDate()}일`
-        const message = `${today}\n누적 가입자 수: ${userCount}명\n누적 플레이리스트 수: ${playlistCount}개\n누적 데일리 수: ${dailyCount}개\n오늘 접속한 유저 수: ${todayAccessedUserCount}명`
+        const prevUserCount = getPrevUserCount()
+        const today = `${prevTime.getFullYear()}년 ${prevTime.getMonth() < 9 ? '0' + (prevTime.getMonth()+1) : prevTime.getMonth()+1}월 ${prevTime.getDate() < 10 ? '0' + prevTime.getDate() : prevTime.getDate()}일`
+        const message = `${today}\n누적 가입자 수: ${userCount}명(+${userCount-prevUserCount})\n누적 플레이리스트 수: ${playlistCount}개\n누적 데일리 수: ${dailyCount}개\n오늘 접속한 유저 수: ${todayAccessedUserCount}명`        
         await slackBot.chat.postMessage({
             channel: channel,
             text: message
         })
+        const writedContents = `${prevTime.getFullYear()}-${prevTime.getMonth() < 9 ? '0' + (prevTime.getMonth()+1) : prevTime.getMonth()+1}-${prevTime.getDate() < 10 ? '0' + prevTime.getDate() : prevTime.getDate()} ${userCount}\n`
+        fs.writeFileSync(userCountPath, writedContents, {flag: 'a+'})
     } catch (err) { 
         res.status(422).send(err.message)
     }
